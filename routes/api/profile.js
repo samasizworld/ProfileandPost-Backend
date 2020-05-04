9d3cb50f52405f2b35e3bad8ba91ded0fbc8022a
samasizworld <viasizbro99@gmail.com>
2020-05-04 13:36:42 +0545

    Backend Done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const request =require('request');
+const config =require('config');
 const router =express.Router();
 const authMiddleware =require('../../middleware/auth');
 const Profile =require('../../models/Profile');
@@ -329,4 +330,35 @@ router
         
     }
 });
+
+//@routes-  GET api/profile/github/:username
+//@desc -   Get user repo from github
+//@acess -  Public
+
+router.get('/github/:username',async (req,res)=>{
+    try {
+        const options ={
+            uri :`https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=
+            ${config.get('githubSecretId')}`,
+            method : 'GET',
+            headers: {'user-agent':'node.js'}
+        };
+
+        request(options,(error,response,body)=>{
+            if(error) console.error(error);
+            if(response.statusCode !== 200){
+                return res.status(404).json({msg:'No Github profile found for this user'});
+            }
+
+            res.json(JSON.parse(body));
+        });
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+        
+    }
+});
+
  module.exports = router;
